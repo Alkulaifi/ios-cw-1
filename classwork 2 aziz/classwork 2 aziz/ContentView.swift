baa7dbb99852f44a3bc744b0c2aa1bdf7f855695
Alkulaifi <100786403+Alkulaifi@users.noreply.github.com>
2022-03-02 19:34:30 +0300

    I added my name

---
+++ classwork 2 aziz/classwork 2 aziz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  classwork 2 aziz
+//
+//  Created by MacBook Pro on 02/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, my name is Aziz!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ classwork 2 aziz/classwork 2 aziz/classwork_2_azizApp.swift
@@ -0,0 +1,17 @@
+//
+//  classwork_2_azizApp.swift
+//  classwork 2 aziz
+//
+//  Created by MacBook Pro on 02/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct classwork_2_azizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
